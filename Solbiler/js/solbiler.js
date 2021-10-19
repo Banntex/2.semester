0c6b3657071b393de6604e7faf570a2e6eaec939
Daniel Kjærgaard <danielrk12345@hotmail.com>
2021-10-19 10:03:20 +0200

    Got the small slideshow to work

--- Solbiler/js/solbiler.js
+++ Solbiler/js/solbiler.js
@@ -91,7 +91,7 @@ function handleSearch(event) {
 
   var rentalPeriodInDays = calculatePeriodInDays();
     
-  fetch("https://api.jsonbin.io/b/61387e669548541c29ae1184/2", {
+  fetch("https://api.jsonbin.io/b/61387e669548541c29ae1184/3", {
     headers: {
       "X-Master-Key":
         "$2b$10$A0AwlK4vKaImkWDDJb2c9.K8W1yZAnbJOJ6R.q6d9/ZCh6YSBBgxm",
@@ -121,3 +121,26 @@ function handleSearch(event) {
 }
 
 searchButton.addEventListener("click", handleSearch);
+  // Dette er vores starts indeks på vores array af billeder
+  var i = 0; 
+  var images = [];
+  var time = 3000;
+
+  // Vores arrayliste af billede
+  images[0] = 'billeder/i1.jpg'
+  images[1] = 'billeder/i2.jpg'
+  images[2] = 'billeder/i3.jpg'
+  images[3] = 'billeder/i4.jpg'
+
+  // Nu vil vi gerne lave en funktion, som skifter vores billeder med starstværdien 0(i)
+  function changeImg(){
+      document.slide.src = images[i];
+      if(i < images.length - 1){
+          i++
+      }
+      else {
+          i = 0;
+      }
+      setTimeout("changeImg()", time);
+  }
+  window.onload = changeImg;
